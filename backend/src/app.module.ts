cd7723fca9ee8aff6b33eb2badae7d0e18e66d03
Le Duy Nam <leduynam005@gmail.com>
2026-04-12 02:09:35 +0700

    hoan thanh cac router user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [],
+  imports: [
+  ConfigModule.forRoot({ isGlobal: true }),
+  UserModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
