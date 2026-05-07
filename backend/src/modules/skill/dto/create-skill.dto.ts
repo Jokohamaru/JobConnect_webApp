547b18f919e21c352c5d4d69dcdc2f788956fed7
Thanhdz <wtfking54@gmail.com>
2026-05-07 17:07:24 +0700

    Xoa folder backend

--- backend/src/modules/skill/dto/create-skill.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateSkillDto {
-  @IsString()
-  @IsNotEmpty({ message: 'Tên kỹ năng không được để trống' })
-  name: string;
-}
